12760587a57cb9ee7bf0d1da8e5f7798f8db42c3
Taher Odeh <taher@aumix.com>
2020-09-16 21:23:04 +0300

    initial

---
+++ trigger.js
@@ -0,0 +1,21 @@
+// import popup from './popup'
+
+/*
+const popup = window.document.createElement('div');
+popup.style.width = "600px";
+popup.style.height = "600px";
+popup.style.position ='absolute';
+popup.style.top= '0px';
+popup.style.right= '0px';
+popup.classList.add("popup-container");
+const app = window.document.createElement('div');
+popup.appendChild(app);
+app.setAttribute("id","app");
+window.document.body.appendChild(popup);
+
+require("./popup");*/
+
+
+const imgCount = window.document.getElementsByTagName("img").length;
+
+alert("Net Mishpat document found with " + imgCount + " images!");
